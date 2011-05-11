5bddbbec1f30cd16970fc72d234c410ee3913e7a
hema0501@163.com <hema0501@163.com@4d6b9eea-9d24-033f-dd66-d003eccfd9d3>
2011-05-11 14:31:14 +0000

    添加GPS数据包解析

--- RailwayDetection/RailwayService/KellService/DataParse/DataPackParse.h
+++ RailwayDetection/RailwayService/KellService/DataParse/DataPackParse.h
@@ -9,10 +9,15 @@ public:
 
 public:
 	// 缺省认为buffer的长度为1024
-	bool PackParse(char* pDataBuffer, GPS_UpLoad_Pack& gpsUpLoadPack);
-	bool PackBuild(char* pDataBuffer, const GPS_DownLoad_Pack& gpsDownLoadPack);
+	bool PackUpLoadParse(const char* pDataBuffer, GPS_UpLoad_Pack& gpsUpLoadPack);		// 解析上行数据包
+	bool PackDownBuild(char* pDataBuffer, const GPS_DownLoad_Pack& gpsDownLoadPack);	// 格式化下行数据包
 
 private:
+	bool ParsePackShell(const char* pDataBuffer, GPS_UpLoad_Pack& gpsUpLoadPack);		// 数据指针指向数据包外壳开始位置
+	bool ParsePackData(const char* pDataBuffer, GPS_UpLoad_Pack& gpsUpLoadPack);		// 数据指针指向数据包中数据区开始位置
+
+	bool BuildPackShell(char* pDataBuffer, const GPS_DownLoad_Pack& gpsDownLoadPack);	// 数据指针指向数据包外壳开始位置
+	bool BuildPackData(char* pDataBuffer, const GPS_DownLoad_Pack& gpsDownLoadPack);	// 数据指针指向数据包中数据区开始位置
 
 };
 
